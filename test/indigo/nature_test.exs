dbd5d1954ef3a8b3b7336b940810f67035884d57
Chris Johnson <cwjohnson42@gmail.com>
2022-09-28 00:55:10 -0700

    Add Nature tests

---
+++ test/indigo/nature_test.exs
@@ -0,0 +1,16 @@
+defmodule NatureTest do
+  use ExUnit.Case, async: true
+  alias Indigo.Nature, as: Nature
+
+  test "list natures" do
+    assert Nature.list() === [
+      %Nature{name: "bashful"},
+      %Nature{name: "brave", inc: :atk, dec: :spd}
+    ]
+  end
+
+  test "get nature" do
+    assert Nature.get("bashful") === %Nature{name: "bashful"}
+    assert Nature.get("brave") === %Nature{name: "brave", inc: :atk, dec: :spd}
+  end
+end
